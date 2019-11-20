147ace5d4385cf848ce9ff4914cf25b77a4d5c26
CatherineVal <katy_vl12@yahoo.com>
2019-11-20 03:55:05 +0000

    Proyecto odoo 12 - Ejercicio openacademy

---
+++ custom_modules/openacademy/models/models.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+# class openacademy(models.Model):
+#     _name = 'openacademy.openacademy'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text()
+	
+	 responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+
+    class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+
+	 instructor_id = fields.Many2one('res.partner', string="Instructor")
+	 domain=[('instructor', '=', True)])
+
+		domain=['|', ('instructor', '=', True),
+                     ('category_id.name', 'ilike', "Teacher")])
+    course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees"
+
+
+	        ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(    
+
+course_id = fields.Many2one('openacademy.course',
+        ondelete='cascade', string="Course", required=True)
+	
+	 attendee_ids = fields.Many2many('res.partner', string="Attendees")
+	
+
+ taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
